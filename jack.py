aebcb6cc18854b0b8d93cf4924b74cb31215f519
EBAG420 <gabeskyland@gmail.com>
2024-08-23 08:23:59 -0700

    exported from Skill Struck

---
+++ jack.py
@@ -0,0 +1,10 @@
+function BubbleSort(list)
+  
+   for all elements in list:
+      if list[i] > list[i+1]
+         swap(list[i], list[i+1])
+      end if
+   end for
+   return list
+end BubbleSort
+   
