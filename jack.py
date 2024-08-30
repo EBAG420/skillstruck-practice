20a8ad5fbe694a474035eeb8e8f6b9997db03b13
EBAG420 <gabeskyland@gmail.com>
2024-08-30 08:07:22 -0700

    exported from Skill Struck

--- jack.py
+++ jack.py
@@ -1,10 +1,13 @@
-function BubbleSort(list)
-  
-   for all elements in list:
-      if list[i] > list[i+1]
-         swap(list[i], list[i+1])
-      end if
-   end for
-   return list
-end BubbleSort
-   
+try: 
+  	shirt = input("What is the color of your shirt?")
+      ear = input('What object is closest to your ear right now?')
+      elbow = input('What object is closest to your elbow right now?')
+      time = input('What were you doing at 5pm yesterday?')
+      sock = ('How many pairs of socks do you own? (Enter as a number)')
+      sock * 10
+except:
+    print('User entered an invalid number')
+else:
+    print('There is no problem')
+finally:
+    print('Your exception handling is complete')
