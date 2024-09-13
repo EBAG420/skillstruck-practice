c57bedc749598f3dbacfc0babaed45ce171c72dd
EBAG420 <gabeskyland@gmail.com>
2024-09-13 08:04:57 -0700

    exported from Skill Struck

--- jack.py
+++ jack.py
@@ -1,13 +1,25 @@
-try: 
-  	shirt = input("What is the color of your shirt?")
-      ear = input('What object is closest to your ear right now?')
-      elbow = input('What object is closest to your elbow right now?')
-      time = input('What were you doing at 5pm yesterday?')
-      sock = ('How many pairs of socks do you own? (Enter as a number)')
-      sock * 10
-except:
-    print('User entered an invalid number')
-else:
-    print('There is no problem')
-finally:
-    print('Your exception handling is complete')
+import pygame
+pygame.init()
+
+x = 500
+y = 500
+
+white = [255, 255, 255]
+black = [0,0,0]
+BLUE = (80,90,250)
+
+Display = pygame.display.set_mode([x,y])
+pygame.display.set_caption('blahblahblah')
+clock = pygame.time.Clock()
+
+rect_x = 50
+rect_y = 50
+
+running = True
+
+while running:
+    Display.fill(BLUE)
+    pygame.draw.rect(Display,white, [rect_x, rect_y 100, 20])
+    rect_x = rect_x + 2
+    rect_y = rect_y + 2
+    clock.tick(60)
